43b9e1852d54366ca19abe36fc4282fd3f82d120
Ilya Pankratov <i.pankratov.main@gmail.com>
2022-10-12 20:28:16 +0300

    Initial ast

--- OCaml-PV/lib/ast.mli
+++ OCaml-PV/lib/ast.mli
@@ -2,6 +2,49 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-type name = string
+type id = string
 
 (** TODO: AST *)
+
+type const =
+  | Int of int
+  | Bool of bool
+  | Nil
+  | String of string
+  | Unit
+
+and bin_op =
+  | Plus
+  | Minus
+  | Mult
+  | Divide
+  | Mod
+  | And
+  | Or
+  | Eq
+  | Neq
+  | Gt
+  | Lt
+  | Gtq
+  | Ltq
+
+and pattern =
+  | PConst of const
+  | PVar of id
+(* Pattern for lists ??? *)
+
+(* We need onaly one arugents in functions, because we carrying
+    For example: let sum x y = x + y
+    Will translate to Let(sum, Var(x), Let("Carry", Var(y), Plus(Var(x), Var(y))))
+       *)
+
+and expr =
+  | IfThenElse of expr * expr * expr
+  | Let of id * expr * expr
+  | LetRec of id * expr * expr
+  | Match of expr * (pattern * expr) list
+  | BinOp of bin_op * expr * expr
+  | Var of id
+  | Fun of id * expr
+  | Apply of expr * expr
+(* Add "::" for Lists? *)
