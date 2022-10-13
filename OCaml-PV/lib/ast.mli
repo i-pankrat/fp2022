056df01c1f2b75f7d5e7e5c0903a73599e4270b1
Ilya Pankratov <i.pankratov.main@gmail.com>
2022-10-13 12:24:13 +0300

    Update ast

--- OCaml-PV/lib/ast.mli
+++ OCaml-PV/lib/ast.mli
@@ -7,11 +7,11 @@ type id = string
 (** TODO: AST *)
 
 type const =
-  | Int of int
-  | Bool of bool
-  | Nil
-  | String of string
-  | Unit
+  | CInt of int
+  | CBool of bool
+  | CNil
+  | CString of string
+  | CUnit
 
 and bin_op =
   | Plus
@@ -31,7 +31,7 @@ and bin_op =
 and pattern =
   | PConst of const
   | PVar of id
-(* Pattern for lists ??? *)
+(* Pattern for lists, tuples ??? *)
 
 (* We need onaly one arugents in functions, because we carrying
     For example: let sum x y = x + y
@@ -39,12 +39,12 @@ and pattern =
        *)
 
 and expr =
-  | IfThenElse of expr * expr * expr
-  | Let of id * expr * expr
-  | LetRec of id * expr * expr
-  | Match of expr * (pattern * expr) list
-  | BinOp of bin_op * expr * expr
-  | Var of id
-  | Fun of id * expr
-  | Apply of expr * expr
-(* Add "::" for Lists? *)
+  | EIfThenElse of expr * expr * expr
+  | ELet of id * expr * expr
+  | ELetRec of id * expr * expr
+  | EMatch of expr * (pattern * expr) list
+  | EBinOp of bin_op * expr * expr
+  | EVar of id
+  | EFun of id * expr
+  | EApply of expr * expr
+(* Add "::" for Lists; add tuples *)
