2649008b5e1a7b9618004ef67de366530a93d670
Ilya Pankratov <i.pankratov.main@gmail.com>
2023-02-05 19:37:31 +0300

    Add interface for interpret

---
+++ OCamlPV/lib/interpret.mli
@@ -0,0 +1,23 @@
+(** Copyright 2022-2023, Ilya Pankratov *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+type 'a binop =
+  | EmptyBinOp of Ast.bin_op
+  | PartialBinOp of 'a * Ast.bin_op
+
+type value =
+  | VInt of int
+  | VBool of bool
+  | VString of string
+  | VTuple of value list
+  | VList of value list
+  | VFun of Ast.pattern * Ast.expr * (Ast.id * value) list
+  | VBinOp of value binop
+  | VUnit
+  | VNil
+
+type ierror
+
+val pp_ierror : Format.formatter -> ierror -> unit
+val run : Ast.expr list -> (value, ierror) result
