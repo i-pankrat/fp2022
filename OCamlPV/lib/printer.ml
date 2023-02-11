9bf1f8eef0470a1715f2ffda3b2fbcdc99f5c01a
Ilya Pankratov <i.pankratov.main@gmail.com>
2023-02-11 13:39:45 +0300

    Add optional arguments to print_program function

--- OCamlPV/lib/printer.ml
+++ OCamlPV/lib/printer.ml
@@ -8,12 +8,12 @@ let pp_result ppf (value, typ) =
   fprintf ppf "- : %a = %a" Pprinttypedtree.pp_typ_letter typ Pprintvalue.pp_value value
 ;;
 
-let print_program program =
+let print_program ?(env_ty = Inferencer.empty) ?(env_inter = Interpret.empty) program =
   match Parser.parse program with
   | Ok ast ->
-    (match Inferencer.check_types ast with
+    (match Inferencer.check_types ~env:env_ty ast with
      | Ok (_, typ) ->
-       (match Interpret.run ast with
+       (match Interpret.run ~env:env_inter ast with
         | Ok (_, res) -> printf "%a" pp_result (res, typ)
         | Error e -> printf "%a" Interpret.pp_ierror e)
      | Error e -> printf "%a" Inferencer.pp_error e)
